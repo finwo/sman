82b61815d0d317748e42d7b981f281b57e81f017
Robin Bron <finwo@pm.me>
2020-11-09 16:41:08 +0100

    Runnable commands

--- src/main.c
+++ src/main.c
@@ -9,6 +9,13 @@
 #define NULL ((void*)0)
 #endif
 
+#ifndef MIN
+#define MIN(a,b) ((a<b)?a:b)
+#endif
+#ifndef MAX
+#define MAX(a,b) ((a>b)?a:b)
+#endif
+
 /* #ifdef __cplusplus */
 /* extern "C" { */
 /* #endif */
@@ -36,7 +43,27 @@ const char *configs[] = {
 
 // Baisc usage
 const char *usage[] = {
-  "sman [options] [[--] args]",
+  "sman --help",
+  "sman [options] command [args]",
+  NULL,
+};
+
+static int commandAdd(int argc, const char **argv, struct configuration *config) {
+  printf("Argc: %d\n", argc);
+  for(int i=0; i<argc; i++) {
+    printf("argument: %s\n", argv[i]);
+  }
+  return 0;
+}
+
+// Which commands we have available
+// fn(argc, argv, config)
+struct {
+  char *name;
+  char *description;
+  int (*fn)(int,const char**,struct configuration *);
+} commands[] = {
+  { "add", "Add a package to the world set and install it", commandAdd },
   NULL,
 };
 
@@ -67,6 +94,7 @@ static int configHandler(void *user, const char *section, const char *name, cons
 
 int main(int argc, const char **argv) {
   int i;
+  const char *command = NULL;
   struct configuration config;
   config.repo      = NULL;
   config.worldfile = "/etc/sman/world";
@@ -80,9 +108,30 @@ int main(int argc, const char **argv) {
     OPT_END(),
   };
 
-  // Parse arguments
+  // Initialize argument parser
   struct argparse argparse;
   argparse_init(&argparse, options, usage, 0);
+
+  // Fetch longest command
+  int longestCommand = 0;
+  for(i=0; commands[i].name; i++) {
+    longestCommand = MAX(longestCommand, strlen(commands[i].name));
+  }
+
+  // Build command list
+  char *tmpstr       = malloc(8192);
+  char *description  = strdup("\nCommands:\n\n");
+  for(i=0; commands[i].name; i++) {
+    sprintf(tmpstr, "    %-*s    %s\n", longestCommand, commands[i].name, commands[i].description);
+    description = realloc(description, strlen(description) + strlen(tmpstr) + 1);
+    strcat(description, tmpstr);
+  }
+  free(tmpstr);
+
+  // Add command list to help
+  argparse_describe(&argparse, "\nOptions:", description);
+
+  // Parse arguments
   argc = argparse_parse(&argparse, argc, argv);
 
   // Load configuration file(s)
@@ -99,7 +148,23 @@ int main(int argc, const char **argv) {
     }
   }
 
-  return 0;
+  // No extra arguments = no command
+  if (argc < 1) {
+    fprintf(stderr, "No command given.\nRun 'sman --help' to see more information.\n");
+    return 1;
+  }
+
+  // Run given command
+  command = *argv;
+  for(i=0; commands[i].name; i++) {
+    if (strcmp(commands[i].name, command) == 0) {
+      return (commands[i].fn)(argc-1, (const char**)(argv+1), &config);
+    }
+  }
+
+  // Still alive = unknown command
+  fprintf(stderr, "Unknown command: %s\nRun 'sman --help' to see more information.\n", command);
+  return 1;
 }
 
 /* #ifdef __cplusplus */
